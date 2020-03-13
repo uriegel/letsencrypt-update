3cd4385a66f0c7684743887f1d0131c87ffd4fd6
Uwe Riegel <uriegel@hotmail.de>
2020-03-13 11:59:57 +0100

    Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UwebServerCert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
