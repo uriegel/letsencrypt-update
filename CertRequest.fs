f295e3849102e02fdb769017b61ac2e222803a8e
Uwe Riegel <uriegel@hotmail.de>
2022-11-19 11:09:59 +0100

    Converted to F#

---
+++ CertRequest.fs
@@ -0,0 +1,12 @@
+// class CertRequest
+// {
+//     public CertRequest(string account, string[] domains, CertificateData data)
+//     {
+//         Account = account;
+//         Domains = domains;
+//         Data = data;
+//     }
+//     public string Account { get; }
+//     public string[] Domains { get; }
+//     public CertificateData Data { get;}
+// }
---
+++ CertificateData.fs
@@ -0,0 +1,20 @@
+// class CertificateData
+// {
+//     public CertificateData(string countryName,string state, string locality, string organization,
+//         string organizationUnit, string commonName)
+//     {
+//         CountryName = countryName;
+//         State = state;
+//         Locality = locality;
+//         Organization = organization;
+//         OrganizationUnit = organizationUnit;
+//         CommonName = commonName;
+//     }
+
+//     public string CountryName { get; }
+//     public string State { get; }
+//     public string Locality { get; }
+//     public string Organization { get; }
+//     public string OrganizationUnit { get; }
+//     public string CommonName { get; }
+// }
